1801cf50979bf48ef0b139adec4c21aafd21ee48
Alexandr Balabanov <alexandr_balabanov55@mail.ru>
2024-10-15 21:44:22 +0500

    Add files via upload

---
+++ module_9_4.py
@@ -0,0 +1,40 @@
+print(f'{"Задача:"} {"«Функциональное разнообразие»"}')
+
+from random import choice
+
+# Lambda-функция:
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+result = list(map(lambda x, y: x == y, first, second))
+print(result)  # [False, True, True, False, False, False, False, False, True, False, False, False, False, False]
+print()
+
+
+# Замыкание:
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for data in data_set:
+                file.write(str(data) + '\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+# Класс с методом call:
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+# Пример использования
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())  # Результат может быть любым из трех вариантов
+print(first_ball())
+print(first_ball())
